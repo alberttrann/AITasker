c9397c1c88e6a881bf00a37d2ab106171e6e4531
chinhan <buiphamchinhan@gmail.com>
2026-06-12 19:19:57 +0700

    [Nhan/12-6] Done set up register function for user

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,18 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { RegisterUserDto } from './dto/register.dto';
 import { PrismaService } from 'prisma/prisma.service';
-@Injectable() // Act as a provider
+import * as bcrypt from 'bcrypt';
+import { UserRoleItem } from '@common/enums/user-role-item.enum';
+import { ActiveRole } from '@common/enums/active-role.enum';
+import { ClientSubType } from '@common/enums/client-subtype.enum';
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async register(registerDto: RegisterUserDto) {
-    // 1. Receive req body from DTO cleanup
-    // 2. Check email existence
-    // 3. Hash password
-    // 4. Create user and save down to db
-  }
+    const existingEmailCheck = await this.prisma.user.findUnique({
+      where: {
+        email: registerDto.email,
+      },
+    });
 
-  async activeRoleMapping(registerDto: RegisterUserDto) {
-    const role = registerDto.roles;
+    if (existingEmailCheck) {
+      throw new ConflictException('Email already exist!');
+    }
+
+    const hashPassword = await bcrypt.hash(registerDto.password, 10);
+    const activeRole =
+      registerDto.roles == UserRoleItem.CLIENT_CEO ? ActiveRole.CLIENT : ActiveRole.EXPERT;
+    const clientSubtype = activeRole == ActiveRole.CLIENT ? ClientSubType.CEO : null;
+
+    // Using transaction to make commit to db only both of queries are successful
+    return await this.prisma.$transaction(async (tx) => {
+      const user = await tx.user.create({
+        data: {
+          email: registerDto.email,
+          passwordHash: hashPassword,
+          fullName: registerDto.fullName,
+          phone: registerDto.phone,
+          roles: [registerDto.roles],
+          activeRole: activeRole,
+          clientSubtype: clientSubtype,
+
+          // Spread operator: take all of the above then create profile align with the activeRole
+          ...(activeRole == ActiveRole.CLIENT
+            ? {
+                clientProfile: { create: {} },
+              }
+            : {
+                expertProfile: { create: {} },
+              }),
+        },
+      });
+
+      const wallet = await tx.wallet.create({
+        data: {
+          userId: user.id,
+        },
+      });
+      return user;
+    });
   }
 }
