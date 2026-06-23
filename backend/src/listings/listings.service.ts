b09118682631a645b6f9a9a2d7a0727a61bdcb8f
caominh74 <tdkmike7406@gmail.com>
2026-06-23 16:38:22 +0700

    [Minh-23/06] Fix: BigInt fix

--- backend/src/listings/listings.service.ts
+++ backend/src/listings/listings.service.ts
@@ -351,7 +351,13 @@ export class ListingsService {
 
     return {
       engagement: result.engagement,
-      virtualAccount: result.va,
+      virtualAccount: {
+        ...result.va,
+        fixedAmount:
+          result.va.fixedAmount !== null
+            ? result.va.fixedAmount.toString()
+            : null,
+      },
       vietqrUrl,
     };
   }
