c838e5f1612dff1892c072220bebb8a05b370ae1
alberttrann <hungminh.2310@gmail.com>
2026-07-03 14:32:43 +0700

    fix button filename

--- frontend/src/components/ui/modal.tsx
+++ frontend/src/components/ui/modal.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { cn } from '@/lib/utils';
 import { X, AlertTriangle, Info } from 'lucide-react';
-import { Button } from './Button';
+import { Button } from './button';
 
 export interface ModalProps {
   isOpen: boolean;
