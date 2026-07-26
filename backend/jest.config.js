410540b5e97a2772a2b36d5336260aa64753da0b
alberttrann <hungminh.2310@gmail.com>
2026-07-27 01:52:02 +0700

    modify jest config

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,13 +1,22 @@
+'use strict';
+
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '.',
-  testRegex: 'test/.*\\.spec\\.ts$',
-  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
   testEnvironment: 'node',
-  moduleDirectories: ['node_modules', '.'],
-  moduleNameMapper: {
-    '^@common/(.*)$':   '<rootDir>/src/common/$1',
-    '^@shared/(.*)$':   '<rootDir>/src/shared/$1',
-    '^@database/(.*)$': '<rootDir>/src/database/$1',
+  // Chỉ định hướng chạy các file Unit test chuẩn
+  testRegex: '(?:src/.*|test/unit/.*)\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
   },
+  maxWorkers: 1,
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^@common/(.*)$': '<rootDir>/src/common/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@database/(.*)$': '<rootDir>/src/database/$1',
+    '^prisma/prisma.service$': '<rootDir>/src/database/prisma.service',
+    '^prisma/(.*)$': '<rootDir>/prisma/$1',
+  },
+  testTimeout: 30_000,
 };
